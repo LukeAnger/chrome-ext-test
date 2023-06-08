d25361e523d44765d1c617ddf8bb7acf8c0491d4
Luke Anger <112991266+LukeAnger@users.noreply.github.com>
2023-06-08 10:37:01 -0400

    changing some styles

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,7 @@
 const div = document.createElement('div');
 div.innerHTML = 'Hello, world!';
+div.id = 'chrome-ext-test';
+div.style.zIndex = 9999;
+div.style.position = 'absolute';
+div.style.fontSize = '72px';
 document.body.appendChild(div);
